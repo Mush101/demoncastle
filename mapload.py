47a02365890838be8237f3a16bb4bb6e721dff35
Mush101 <34507479+Mush101@users.noreply.github.com>
2019-03-25 16:08:02 +0000

    Entity loading from map!

--- mapload.py
+++ mapload.py
@@ -31,21 +31,32 @@ else:
                 num = 0
             new_tiles.append(num)
 
-        out_str = num_to_char(data["layers"][0]["width"])
+        width = data["layers"][0]["width"]
+        out_str = num_to_char(width)
+
+        try:
+            entities = data["layers"][1]["objects"]
+            for e in entities:
+                object_id = e["properties"]["object_id"]
+                hard_mode_only = e["properties"]["hard_mode_only"]
+                x = math.floor(e["x"]/8)
+                y = math.floor(e["y"]/8)
+                tile_pos = x + y * width
+
+                add_number = 256
+                if hard_mode_only:
+                    add_number = 512
+
+                new_tiles[tile_pos] += add_number
+
+                out_str += num_to_one_char(object_id)
+        except IndexError:
+            print("Index error.")
+            pass
+
+        out_str += "|"
+
         for i in range(0, len(new_tiles)):
             out_str += num_to_char(new_tiles[i])
-
-        # try:
-        #     enemies=data["layers"][2]["objects"]
-        #     for e in enemies:
-        #         if e["properties"]["enemy"]=="goblin":
-        #             out_str += "0"
-        #         else:
-        #             out_str += "1"
-        #         out_str+=num_to_one_char(e["properties"]["level"])
-        #         out_str+=num_to_one_char(math.floor(e["x"]/4))
-        #         out_str+=num_to_one_char(math.floor(e["y"]/4))
-        # except IndexError:
-        #     pass
 
         print(out_str)
